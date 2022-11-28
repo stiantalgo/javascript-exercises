b7cf3289e2f27b4886967d61f7bd6a38de586f16
stiantalgo <stiantalgo@gmail.com>
2022-11-28 12:15:11 +0100

    3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(myString, num) {
+    if(num > 0){
+        return myString.repeat(num);
+    }
+    else if(num == 0 || myString == ""){
+        return "";
+    }
+    else{
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseWord = "";
+    for(let i = (str.length-1); i >= 0; i--){
+        reverseWord += str[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
